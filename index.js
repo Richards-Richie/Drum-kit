a3d534d1dbf47c673ca371ccc38a46cb893900a5
Richards-Richie <danderichards360@gmail.com>
2022-09-29 09:12:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,31 @@ for(var i=0;i<a;i++){
     document.querySelectorAll("button")[i].addEventListener("click",clickfun);
 }
 function clickfun(){
-    this.style.color="white"
-    audio=new Audio("./sounds/tom-1.mp3");
-    audio.play();
+    switch(this.innerHTML){
+        case "w":
+            var audio=new Audio("./sounds/crash.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio=new Audio("./sounds/kick-bass.mp3");
+            audio.play();
+        case "s":
+            var audio=new Audio("./sounds/snare.mp3");
+            audio.play();
+        case "d":
+            var audio=new Audio("./sounds/tom-1.mp3");
+            audio.play();
+        case "j":
+            var audio=new Audio("./sounds/tom-2.mp3");
+            audio.play();
+        case "k":
+            var audio=new Audio("./sounds/tom-3.mp3");
+            audio.play();
+        case "l":
+            var audio=new Audio("./sounds/tom-4.mp3");
+            audio.play();
+        default:
+            break;
+        }
 }
 //var 
